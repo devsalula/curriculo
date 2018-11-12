f0a353b0f515be3b09764fd60addd8650b8869ed
devsalula <saleh.nazih.dev@gmail.com>
2018-11-12 00:42:44 -0200

    Add color in sidenav and titles

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -7,11 +7,25 @@ class Sidebar extends Component {
     render(){
         return(
             <div>
-                <ul id="slide-out" className="sidenav sidenav-fixed">
-                    <li><a href="#!">First Sidebar Link</a></li>
-                    <li><a href="#!">Second Sidebar Link</a></li>
-                    </ul>
-                    <a href="#" data-target="slide-out" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+                <ul id="slide-out" className="sidenav sidenav-fixed grey darken-4">
+                    <div>
+                        <li>
+                            <div className="user-view">
+                                <div className="background">
+                                    <img src="../../public/assets/images/starsblue.jpeg" />
+                                </div>
+                                <a><img className="circle" src="" /></a>
+                                <a><span className="white-text name">Saleh Nazih</span></a>
+                                <a><span className="white-text email">saleh.nazih.dev@gmail.com</span></a>
+                            </div>
+                        </li>
+                        <li><a href="#!"><span className="nav-font">Quem sou eu?</span></a></li>
+                        <li><a href="#!"><span className="nav-font">Formação Acadêmica</span></a></li>
+                        <li><a href="#!"><span className="nav-font">Experiência</span></a></li>
+                        <li><a href="#!"><span className="nav-font">Projetos</span></a></li>
+                        <li><a href="#!"><span className="nav-font">Conhecimentos</span></a></li>
+                    </div>
+                </ul>
             </div>
         );
     }
